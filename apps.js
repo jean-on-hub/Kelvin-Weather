f43e0f23da419ef7133c980c405f5ef773b50d49
k2908 <56277386+k2908@users.noreply.github.com>
2022-02-02 19:09:50 +0000

    project

---
+++ apps.js
@@ -0,0 +1,14 @@
+// today's forecast in kelvin
+const kelvin = 0;
+// celsius read
+const celsius = kelvin-273;
+// convert to faherenheit
+let Faherenheit = Math.floor(celsius *(9/5) +32);
+
+console.log(`The temperature is ${Faherenheit} degrees Fahrenheit.`);
+
+
+let Newton = Math.floor(celsius*(33/100))
+
+console.log(`The temperature is ${Newton} degrees Celsius.`);
+
